907a10f957da222b43e59301b61dab13519b4d98
Cloud User <centos@ip-172-31-18-213.ap-southeast-1.compute.internal>
2020-06-01 15:50:37 +0000

    Incomplete solution to Ex3

--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -13,16 +13,60 @@
 module top_tb(
     );
     
-//Todo: Parameters
+	//Todo: Parameters
+	parameter CLK_PERIOD = 10;
 
-//Todo: Regitsers and wires
+	//Todo: Regitsers and wires
+	reg clk;
+    reg rst;
+	reg enable;
+	reg direction;
+	reg err;
+	wire [7:0] counter_out;
 
-//Todo: Clock generation
+	//Todo: Clock generation
+	initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
 
-//Todo: User logic
-    
-//Todo: Finish test, check for success
+	//Todo: User logic
+	initial begin
+    	rst=1; // start with reset so can check from there
+		enable=0;
+		direction=0;
+    	err=0;
+		forever begin
+			#CLK_PERIOD
+			// No reset, With enable, Still at Zero // positive direction give 11111111 and vice versa
+        	if ((rst&(counter_out!=8'b00000000))|((!rst)&enable&(counter_out=8'b00000000))|(direction&(counter_out=8'b11111111))|(!direction&(counter_out=8'b00000001)))
+			begin
+				$display("***TEST FAILED!");
+				err=1;
+         	end
+		    rst=rst+1;
+		    if (rst==1)
+		    	enable=~enable;
+		end
+	end
+	//Todo: Finish test, check for success
+	initial begin
+		// 8 different permutations to test
+        #100 
+        if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+      end
 
-//Todo: Instantiate counter module
- 
+	//Todo: Instantiate counter module
+ 	counter top (
+	.clk (clk),		
+	.rst (rst),
+	.enable (enable),
+	.direction (direction),
+	.counter_out (counter_out)
+	);
+
 endmodule 
