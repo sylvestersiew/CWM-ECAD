916194fa3b3c5f36ca607e807aece7389aa24279
Cloud User <centos@ip-172-31-18-213.ap-southeast-1.compute.internal>
2020-06-03 08:58:07 +0000

    A complete solution to Ex5

--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -9,116 +9,92 @@
 `timescale 1ns / 100ps
 
 module top_tb(
-    );
+	);
     
 	//Todo: Parameters
 	parameter CLK_PERIOD = 10;
 
 	//Todo: Regitsers and wires
 	reg clk;
-	reg [3:0] RAG_before;
-	wire [3:0] RAG;
+	reg err;
+	reg [2:0] RAG_before;	
+	wire [2:0] RAG;
+	wire red;
+	wire amber;
+	wire green;
+
+	assign RAG[2] = red;
+	assign RAG[1] = amber;
+	assign RAG[0] = green;
 
 	//Todo: Clock generation
-	initial
-    begin
-       clk = 1'b0;
-       forever
-         #(CLK_PERIOD/2) clk=~clk;
-     end
+	initial	begin
+  		clk = 1'b0;
+  		forever
+  			#(CLK_PERIOD/2) clk=~clk;
+  	end
 
 	//Todo: User logic
 	initial begin
-		button=1'b0;
-    	err=0;
-    	RAG_before=3'b000;
-    	RAG=3'b000; // Initialise value for CODE below to work with = IMMEDIATELY gives a new value
+		
+		err=0;
+		RAG_before=3'b000;
     	
 		forever begin
-			// at start, RAG = new value, RAG b4 = old value
+			
 			#CLK_PERIOD
 			
-        	if(RAG_before==3'b000|RAG_before==3'b101|RAG_before==3'b111|RAG_before==3'b011)
-	    	begin
-				if (RAG_before!=3'b000|RAG==3'b101|RAG==3'b111|RAG==3'b011)
+			if ((RAG_before==3'b000||RAG_before==3'b101||RAG_before==3'b111||RAG_before==3'b011) && (RAG==3'b000||RAG==3'b101||RAG==3'b111|RAG==3'b011))
 				begin
-					$display("***TEST FAILED!,001 to 110 button 0 issue***");
+					$display("***TEST FAILED!,unable to self start***");
 					err=1;
 				end
-			end
 			
-        	begin
-				if (throw!=3'b000)
+			else if ((RAG_before==3'b100) && (RAG!=3'b110))
 				begin
-					$display("***TEST FAILED!,Reset issue***");
+					$display("***TEST FAILED!,100->110 issue***");
 					err=1;
 				end
-			end
-				
-			else if (throw_before==3'b000|throw_before==3'b111)
-			begin
-					if (throw != 3'b001)
-					begin
-						$display("***TEST FAILED!,000 and 111 issue***");
-						err=1;
-					end
-			end
-					
-			else if (!button)
-			begin
-				if (throw!=throw_before)
+
+			else if ((RAG_before==3'b110) && (RAG!=3'b001))
 				begin
-					$display("***TEST FAILED!,001 to 110 button 0 issue***");
+					$display("***TEST FAILED!,110->001 issue***");
 					err=1;
-				end
-			end
+				end	
+
+			else if ((RAG_before==3'b001) && (RAG!=3'b010))
+				begin
+					$display("***TEST FAILED!,001->010 issue***");
+					err=1;
+				end	
 			
-			else if (throw_before==3'b110)
-			begin
-				if (throw!=throw_before)
+         		else if ((RAG_before==3'b010) && (RAG!=3'b001))
 				begin
-					$display("***TEST FAILED!,110 button 1 issue***");
+					$display("***TEST FAILED!,001->010 issue***");
 					err=1;
 				end
-			// from 001 to 101, wrt button 1
-			end
-			
-			else 
-			begin
-				if (throw!=(throw_before+1))
-				begin
-					$display("***TEST FAILED!,001 to 101 button 1 issue***");
-					err=1;
-				end
-         	end
-         	
-         	begin
-         	// at end, RAG b4 = new value becomes old value
-		    button=button+1;  //2-000 4-010 6-001 8-011
-			// test reset
-			#50
-			rst <= 1;
-			#70
-			rst <= 0;
-			end
+
+			// at end, RAG b4 = new value becomes old value
+			RAG_before = RAG;
+
 		end
 	end
 	
 	//Todo: Finish test, check for success
 	initial begin
-		// From 70s, time to complete one loop of testing
-        #220
-        if (err==0)
-          $display("***TEST PASSED! :) ***");
-        $finish;
-      end
+	// 40s, time to complete one loop of testing
+        	#50
+        	if (err==0)
+          		$display("***TEST PASSED! :) ***");
+		$finish;
+	end
 
-	//Todo: Instantiate counter module
-	mux traffic (
-    .clk (clk),
-    .red (RAG[2]),
-    .amber (RAG[1]),
-    .green (RAG[0])
-    );
+	//Todo: Instantiate traffic module
+	traffic top (
+	.clk (clk),
+	.red (red),
+	.amber (amber),
+	.green (green)
+	);
 
 endmodule 
