adde578a54bf13c107b49d9cc1b2163ab6ce9a72
Cloud User <centos@ip-172-31-18-213.ap-southeast-1.compute.internal>
2020-06-04 13:41:11 +0000

    Complete Ex8

--- Ex8/top.v
+++ Ex8/top.v
@@ -15,6 +15,7 @@
 
 module timestable(
 	input clk,
+	input rst,
 	input [2:0] a,
 	input [2:0] b,	
 	input read,
@@ -22,30 +23,52 @@ module timestable(
 	);
 
 	wire [5:0] ab;
+	
+	// Unused Outputs
+	wire s_axi_rresp;
+	wire s_axi_rvalid;
+	wire rsta_busy;
+	wire rstb_busy;
+	wire s_axi_awready;
+	wire s_axi_wready;
+	wire s_axi_bresp;
+	wire s_axi_bvalid;
+	wire s_axi_arready;
+
 	assign ab = {a[2:0],b[2:0]};
 
 	blk_mem_gen_AXI4 your_instance_name (
+	  // Safety Circuit not required
 	  .rsta_busy(rsta_busy),          // output wire rsta_busy
 	  .rstb_busy(rstb_busy),          // output wire rstb_busy
-	  .s_aclk(s_aclk),                // input wire s_aclk
-	  .s_aresetn(s_aresetn),          // input wire s_aresetn
-	  .s_axi_awaddr(s_axi_awaddr),    // input wire [31 : 0] s_axi_awaddr
-	  .s_axi_awvalid(s_axi_awvalid),  // input wire s_axi_awvalid
+	  // Global Clock and Reset
+	  .s_aclk(clk),                // input wire s_aclk
+	  .s_aresetn(rst),          // input wire s_aresetn
+	  // Write I/O unused
+	  .s_axi_awaddr(0),    // input wire [31 : 0] s_axi_awaddr
+	  .s_axi_awvalid(0),  // input wire s_axi_awvalid
 	  .s_axi_awready(s_axi_awready),  // output wire s_axi_awready
-	  .s_axi_wdata(s_axi_wdata),      // input wire [31 : 0] s_axi_wdata
-	  .s_axi_wstrb(s_axi_wstrb),      // input wire [3 : 0] s_axi_wstrb
-	  .s_axi_wvalid(s_axi_wvalid),    // input wire s_axi_wvalid
+	  .s_axi_wdata(0),      // input wire [31 : 0] s_axi_wdata
+	  .s_axi_wstrb(0),      // input wire [3 : 0] s_axi_wstrb
+	  .s_axi_wvalid(0),    // input wire s_axi_wvalid
 	  .s_axi_wready(s_axi_wready),    // output wire s_axi_wready
 	  .s_axi_bresp(s_axi_bresp),      // output wire [1 : 0] s_axi_bresp
 	  .s_axi_bvalid(s_axi_bvalid),    // output wire s_axi_bvalid
-	  .s_axi_bready(s_axi_bready),    // input wire s_axi_bready
-	  .s_axi_araddr(s_axi_araddr),    // input wire [31 : 0] s_axi_araddr
-	  .s_axi_arvalid(s_axi_arvalid),  // input wire s_axi_arvalid
+	  .s_axi_bready(0),    // input wire s_axi_bready
+	  // Read Address
+	  .s_axi_araddr(ab),    // input wire [31 : 0] s_axi_araddr
+	  // When High address is valid
+	  .s_axi_arvalid(read),  // input wire s_axi_arvalid
+	  // Readiness of slave to receive above address is irrelevant as we will see from output
 	  .s_axi_arready(s_axi_arready),  // output wire s_axi_arready
-	  .s_axi_rdata(s_axi_rdata),      // output wire [31 : 0] s_axi_rdata
+	  // Output (read) data
+	  .s_axi_rdata(result),      // output wire [31 : 0] s_axi_rdata
+	  // Not-needed Output as ID not used
 	  .s_axi_rresp(s_axi_rresp),      // output wire [1 : 0] s_axi_rresp
+	  // Readiness to send to Master is irrelevant as we will see from output
 	  .s_axi_rvalid(s_axi_rvalid),    // output wire s_axi_rvalid
-	  .s_axi_rready(s_axi_rready)    // input wire s_axi_rready
+	  // Master ready to receive if ENABLED
+  	  .s_axi_rready(read)    // input wire s_axi_rready
 	);
 
-
+endmodule
--- Ex8/top_tb.v
+++ Ex8/top_tb.v
@@ -6,4 +6,83 @@
 // Description: A testbench module to test Ex8 - Times Table using AXI4-Lite
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+	);
+    
+	//Todo: Parameters
+	parameter CLK_PERIOD = 10;
+
+	//Todo: Regitsers and wires
+	reg clk;
+	reg rst;
+	reg err;
+	reg [2:0] a;	
+	reg [2:0] b;
+	reg read;
+	reg result_prev;
+	wire [5:0] result;
+	
+	//Decide how enable will be tested	
+	initial begin
+		read=1;
+		#391 // If put at 140, test will be for read=0 instead of the results on read=1 at clock #135 instance
+		read=0;
+	end
+	
+	//Todo: Clock generation
+	initial	begin
+  		clk = 1'b0;
+  		forever
+  			#(CLK_PERIOD/2) clk=~clk;
+  	end
+
+	//Todo: User logic
+	initial begin
+		a=0;
+		b=0;
+		rst = 0;
+		err=0;
+		result_prev=0;
+		forever begin
+			
+			#CLK_PERIOD
+			if (read && a*b!=result)
+				begin
+					$display("***TEST FAILED!,wrong calculation***");
+					err=1;
+				end
+
+			else if (!read && result_prev!=result)
+				begin
+					$display("***TEST FAILED!,read issue***");
+					err=1;
+				end
+			result_prev = result;	
+			a = a+1;
+			if (a==0)
+				b = b+1;
+		end
+	end
+	
+	//Todo: Finish test, check for success
+	initial begin
+	// 7 ticks, time to complete one of seven of testing
+        	#400
+        	if (err==0)
+          		$display("***TEST PASSED! :) ***");
+		$finish;
+	end
+
+	//Todo: Instantiate traffic module
+	timestable top (
+	.clk (clk),
+	.rst (rst),
+	.a (a),
+	.b (b),
+	.read (read),
+	.result (result)
+	);
+	
+endmodule 
