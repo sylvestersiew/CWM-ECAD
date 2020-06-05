8c2a59fcd3c454e69abb9b4af474f4c72d8e98c0
Cloud User <centos@ip-172-31-18-213.ap-southeast-1.compute.internal>
2020-06-05 09:57:29 +0000

    Complete Ex9

--- Ex9/constraints.xdc
+++ Ex9/constraints.xdc
@@ -16,6 +16,20 @@ set_property BITSTREAM.CONFIG.SPI_FALL_EDGE YES        [current_design]
 
 #############################################################################################################
 
-
-
+set_property PACKAGE_PIN AY38 [get_ports {clk_n}]
+set_property IOSTANDARD DIFF_SSTL12 [get_ports {clk_n}]
+set_property PACKAGE_PIN AY37 [get_ports {clk_p}]
+set_property IOSTANDARD DIFF_SSTL12 [get_ports {clk_p}]
+create_clock -name clk -period 10 [get_ports {clk_p}]
+set_property PACKAGE_PIN BD21 [get_ports {rst_n}]
+set_property IOSTANDARD LVCMOS18 [get_ports {rst_n}]
+set_property PULLUP true [get_ports {rst_n}]
+set_property PACKAGE_PIN BC21 [get_ports {led_0}]
+set_property IOSTANDARD LVCMOS18 [get_ports {led_0}]
+set_property PACKAGE_PIN BB21 [get_ports {led_1}]
+set_property IOSTANDARD LVCMOS18 [get_ports {led_1}]
+set_property PACKAGE_PIN BA20 [get_ports {led_2}]
+set_property IOSTANDARD LVCMOS18 [get_ports {led_2}]
+set_property PACKAGE_PIN AL20 [get_ports {button}]
+set_property IOSTANDARD LVCMOS18 [get_ports {button}]
 
--- Ex9/top.v
+++ Ex9/top.v
@@ -12,16 +12,26 @@
 //  outputs:
 //           led_0, led_1, led_2
 //////////////////////////////////////////////////////////////////////////////////
-
-
 module top(
-    input clk_p,
-    input clk_n,
-     //Todo: add all other ports besides clk_n and clk_p 
-   );
-    
-
-   /* clock infrastructure, do not modify */
+	input clk_p,
+	input clk_n,
+	//Todo: add all other ports besides clk_n and clk_p
+	input rst_n,
+	input button,
+	output led_2,
+	output led_1,
+	output led_0
+	);
+	
+	// Count for 2,000,000 - 50Hz blink cycle vs 100MHz clock cycle
+	reg [23:0] counter = 24'h0;
+	reg [2:0] led;
+	
+	assign led_2 = led[2];
+	assign led_1 = led[1];
+	assign led_0 = led[0];
+	
+	/* clock infrastructure, do not modify */
         wire clk_ibufds;
 
     IBUFDS IBUFDS_sysclk (
@@ -37,6 +47,24 @@ module top(
 	.O  (clk)
       );
 
-//Add logic here
-
+	//Add logic here
+	always@(posedge clk or posedge rst_n)
+	begin
+		// Reset is Active Low, Button pressed, LED to be off
+		if(!rst_n||button==1) 
+			begin
+			led = 3'b000;
+			counter = 24'h0;
+			end
+		// LED on once no reset or button
+		else if (counter==24'h1)
+			led = 3'b111;
+		// off half a cycle later for "blinking"
+		else if(counter==24'd1000001)
+			led = 3'b000;
+		else
+			;
+		counter=counter+1;
+	end
+	
 endmodule
